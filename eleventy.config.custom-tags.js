e2f7aed961141ee8f999814a345a46ead7c666d5
Sergey Golovin <sergey.golovin@protonmail.com>
2023-02-28 11:18:42 +0300

    add content, add links into the menu

--- eleventy.config.custom-tags.js
+++ eleventy.config.custom-tags.js
@@ -1,7 +1,7 @@
 module.exports = eleventyConfig => {
-  eleventyConfig.addPairedShortcode("accordion", function(content, title) {
+  eleventyConfig.addPairedShortcode("accordion", function(content, title, id = '') {
     return `<details class="accordion">
-  <summary class="accordion__summary">
+  <summary class="accordion__summary" ${id ? 'id="' + id + '"' : ''}>
     ${title}
   </summary>
   <div class="accordion__text">
