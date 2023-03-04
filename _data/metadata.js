d9cb8f897f5a9dcb08ea9c1fd7fb11c68b0e5e1f
Sergey Golovin <sergey.golovin@protonmail.com>
2023-03-04 11:01:58 +0300

    add description and avif images

--- _data/metadata.js
+++ _data/metadata.js
@@ -2,4 +2,5 @@ module.exports = {
 	"title": "Space Twistie",
 	"url": "https://spacetwistie.com/",
 	"language": "en",
+	"description": "Space Twistie develops custom web applications of any complexity to meet your unique business needs. We specialize in outsourcing services and provide expert assistance to help you achieve your goals.",
 }
