9aadd6787f17c1f8170b34a8a448da6fe414f50e
Sergey Golovin <sergey.golovin@protonmail.com>
2023-03-08 16:46:54 +0300

    move social meta imformation to metadata.js

--- _data/metadata.js
+++ _data/metadata.js
@@ -3,4 +3,11 @@ module.exports = {
 	"url": "https://spacetwistie.com/",
 	"language": "en",
 	"description": "Space Twistie develops custom web applications of any complexity to meet your unique business needs. We specialize in outsourcing services and provide expert assistance to help you achieve your goals.",
+	"social": {
+		"siteName": "Space Twistie",
+		"title": "Space Twistie   |   Expertise. Transparency. Quality.",
+		"description": "Developing custom web applications of any complexity to meet your unique business needs. We specialize in outsourcing and provide expert assistance to you.",
+		"preview": "/img/preview.png",
+		"url": "https://spacetwistie.com/",
+	}
 }
